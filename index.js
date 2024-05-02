b77044a01e2cd4168575631bae4f6711428026e6
GloryEvi <167655575+GloryEvi@users.noreply.github.com>
2024-05-02 04:44:31 +0100

    Inject the forecast Html from Javascript

--- index.js
+++ index.js
@@ -47,8 +47,32 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["thu", "fri", "sat", "sun", "mon"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌦
+    </div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">18°</span>
+      <span class="weather-forecast-temperature-min">12°</span>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lagos");
+displayForecast();
